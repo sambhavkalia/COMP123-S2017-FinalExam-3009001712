d941405c800d80119a64ef5b4f0253fd1bd495f2
sambhavkalia <sambhavkalia@gmail.com>
2017-08-17 10:55:08 -0400

    added splash form with its properties and timer.

--- COMP123-S2017-FinalExam-3009001712/Program.cs
+++ COMP123-S2017-FinalExam-3009001712/Program.cs
@@ -24,7 +24,7 @@ namespace COMP123_S2017_FinalExam_3009001712
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new PickHighestCardForm());
+            Application.Run(new SplashForm());
         }
     }
 }
---
+++ COMP123-S2017-FinalExam-3009001712/SplashForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2017_FinalExam_3009001712
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            {
+                timer1.Enabled = false;
+                this.Hide();
+                PickHighestCardForm pick = new PickHighestCardForm();
+                pick.Show();
+            }
+        }
+    }
+}
